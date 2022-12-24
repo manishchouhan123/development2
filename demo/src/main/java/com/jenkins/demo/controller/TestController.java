7f0b0f9a7719015afd73ce7cac6408f81d74b702
manishchouhan123 <itsmanish61@gmail.com>
2022-12-24 21:31:58 +0530

    first commit

---
+++ demo/src/main/java/com/jenkins/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/printRequest")
+	public ResponseEntity<String> printRequest(@RequestParam String name){
+		System.out.println("Name =================="+name);
+		return new ResponseEntity<String>(name , HttpStatus.OK);
+	}
+
+}
